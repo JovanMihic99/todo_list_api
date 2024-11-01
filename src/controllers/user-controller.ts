291597b295be9f62ff8a8c92d9fbd8d5184978ff
JovanMihic99 <jovanmihic99@gmail.com>
2024-11-01 14:32:43 +0100

    feat: add authentication to delete route and remove redundant add_user route

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -4,25 +4,6 @@ import asyncHandler from "express-async-handler";
 const prisma = new PrismaClient();
 const user = prisma.user;
 
-// CREATE
-const add_user = asyncHandler(async (req, res) => {
-  const name = req.params.name;
-  const email = req.params.email;
-  const password = req.params.password; // add hashing later
-  const data = {
-    name,
-    email,
-    password,
-  };
-  const user = await prisma.user.create({
-    data,
-  });
-
-  res
-    .status(200)
-    .json({ message: `Succesfully added User with id: ${user.id}`, user });
-});
-
 // READ
 const get_users = asyncHandler(async (req, res) => {
   const data = await prisma.user.findMany();
@@ -41,7 +22,7 @@ const get_user_by_id = asyncHandler(async (req, res) => {
 
 // DELETE
 const delete_user = asyncHandler(async (req, res) => {
-  const userId = parseInt(req.params.userId);
+  const userId = parseInt(req.user.id);
   const deleteTasks = prisma.task.deleteMany({
     where: {
       userId: userId,
@@ -54,11 +35,14 @@ const delete_user = asyncHandler(async (req, res) => {
   });
 
   const transaction = await prisma.$transaction([deleteTasks, deleteUser]);
+  res.status(200).json({
+    message: `Successfully removed user with id ${userId}`,
+    removedUser: req.user,
+  });
 });
 
 export default {
   get_users,
   get_user_by_id,
-  add_user,
   delete_user,
 };
--- src/routes/v1/user-router.ts
+++ src/routes/v1/user-router.ts
@@ -11,12 +11,10 @@ router.get("/", userController.get_users);
 router.get("/:id", userController.get_user_by_id);
 
 // POST
-router.post("/", userController.add_user);
-
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 // DELETE
-router.delete("/:userId", userController.delete_user);
+router.delete("/", auth.authenticate, userController.delete_user);
 
 export default router;
